6e5513ee61bcb8b0883e9034bfa5864f5764f4e8
William Higgins <515885+higs4281@users.noreply.github.com>
2025-08-20 23:28:42 -0400

    fix squashed migration dependency reference

--- background_task/migrations/0003_alter_completedtask_id_alter_task_id.py
+++ background_task/migrations/0003_alter_completedtask_id_alter_task_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('background_task', '0004_auto_20220202_1721'),
+        ('background_task', '0002_auto_20170927_1109'),
     ]
 
     operations = [
